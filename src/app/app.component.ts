ee4141d4181ad705bb91cbf11483043b90b5257d
Oscar rondon <oscarrondon77@gmail.com>
2024-06-28 18:46:10 +0200

    Maqueta inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListaDeNoticiasComponent } from './components/lista-de-noticias/lista-de-noticias.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormularioComponent,ListaDeNoticiasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
