13374ab0269a26c0108709a3310b413b5443b695
Oscar rondon <oscarrondon77@gmail.com>
2024-06-28 18:28:11 +0200

    creacion de componentes formulario y lista de noticias

---
+++ src/app/components/lista-de-noticias/lista-de-noticias.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-de-noticias',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-de-noticias.component.html',
+  styleUrl: './lista-de-noticias.component.css'
+})
+export class ListaDeNoticiasComponent {
+
+}
