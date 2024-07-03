91073c4c3eb348f74c894becb103692e9d923c1f
Oscar rondon <oscarrondon77@gmail.com>
2024-07-03 21:07:13 +0200

    chore: actualizar manejo de datos en formulario y lista de noticias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,15 @@
 import { Component } from '@angular/core';
 import { FormularioComponent } from './components/formulario/formulario.component';
-import { ListaDeNoticiasComponent } from './components/lista-de-noticias/lista-de-noticias.component';
-import { Iformulario } from './interfaces/iformulario.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormularioComponent,ListaDeNoticiasComponent],
+  imports: [FormularioComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  arrForm: Iformulario[] = [{
-    title: 'hola mundo',
-    imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdatademia.es%2Fblog%2Fhello-world&psig=AOvVaw1PZCTM-Vl1vZebxXuvprPW&ust=1719918343971000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCMDC_qHZhYcDFQAAAAAdAAAAABAE',
-    body: 'el mundo es mundisimo mundo',
-    date: new Date
-  },
-  {
-    title: 'hola world',
-    imageUrl: 'https://es.wikipedia.org/wiki/Hola_mundo#/media/Archivo:Holamundoc++.png',
-    body: 'helllllooooooooooooooooooooo',
-    date: new Date
-  }
-];
-
+  
+  
 
 }
--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,14 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
 import { Iformulario } from '../../interfaces/iformulario.interface';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  pepe: string = 'hola mundo';
+arrNoticia: Iformulario[] = [
+  {title: 'hola mundo',imageUrl:"https://www.aon.es/personales/seguro-perro-gato/wp-content/uploads/sites/2/2021/04/bichon-maltes.jpg",body:"el mundo es mundisimo mundo",date:new Date()},
+  {title: 'hola world',imageUrl:"https://www.aon.es/personales/seguro-perro-gato/wp-content/uploads/sites/2/2021/04/bichon-maltes.jpg",body:"helllllooooooooooooooooooooo",date:new Date()}
+];
+  newNotice: Iformulario = {
+    title: '',
+    imageUrl: '',
+    body: '',
+    date: new Date()
+  };
+  enviarNoticia(){
+    if(this.newNotice.title === '' || this.newNotice.imageUrl === '' || this.newNotice.body === '') {return alert('Faltan datos')}
+    this.arrNoticia.push(this.newNotice)
+    this.newNotice = {
+      title: '',
+      imageUrl: '',
+      body: '',
+      date: new Date()
+    }
+  }
+  cargarNoticias():string{
+    let html =` `
+    this.arrNoticia.forEach((noticia)=>{
+      html += `<ul>
+        <li >
+            <h2>${noticia.title}</h2>
+            <img src="${noticia.imageUrl}">
+            <p>${noticia.body}</p>
+            <small>${noticia.date}</small>
+          </li>
+        </ul>`
+    })
+        return html
+    }
+    
+    } 
 
-  
-}
+
+
+
